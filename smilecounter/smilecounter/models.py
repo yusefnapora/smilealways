27d76d15d5d3415b6e4b99a6001869eea3fcd64f
Josh haimson <jdhaimson@gmail.com>
2013-11-10 14:07:32 -0500

    Basic counter working

--- smilecounter/smilecounter/models.py
+++ smilecounter/smilecounter/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.core.exceptions import ObjectDoesNotExist
 
 class SmileRedirect(models.Model):
     time = models.DateTimeField()
--- smilecounter/smilecounter/views.py
+++ smilecounter/smilecounter/views.py
@@ -1,4 +1,16 @@
-from django.shortcuts import get_object_or_404, render, redirect
+from django.shortcuts import render
+from django.http import HttpResponse
+from smilecounter.models import SmileRedirect
+from datetime import datetime
+from pytz import UTC
+from django.utils import simplejson
 
 def home(request):
-    return render(request, 'smilecounter/index.html', {})
+    redirectCount = SmileRedirect.objects.count()
+    print redirectCount
+    return render(request, 'smilecounter/index.html', {'redirectCount':redirectCount})
+
+def addRedirect(request):
+    SmileRedirect.objects.create(time=UTC.localize(datetime.now()))
+    response_data = {}
+    return HttpResponse(simplejson.dumps(response_data), content_type="application/json")
