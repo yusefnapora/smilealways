063281400879fe6bbcbf8734ff0034bc0795cc25
Josh haimson <jdhaimson@gmail.com>
2013-11-10 12:57:00 -0500

    Don't redirect pages that will already be redirected

--- chromeplugin/request.js
+++ chromeplugin/request.js
@@ -10,6 +10,8 @@ function detectRedirect(details) {
     var https = "https://";
     var amazonurl = "www.amazon.com";
     var smileurl = "smile.amazon.com";
+    // string that all amazon redirect urls contain
+    var redirecturl = "redirect.html";
     
     if (url != null) {
 // Debug code
@@ -18,20 +20,25 @@ if (bkg != null) {
     bkg.console.log(url.match(amazonurl));
 }
 // END DEBUG CODE:
-        // Check non-secure links
-        if(url.match(http + amazonurl) != null) {
-            return{
-                // redirect to amazon smile append the rest of the url
-                redirectUrl : http + smileurl + url.split(amazonurl)[1]
-            };
-        }
 
-        // Check secure links
-        else if (url.match(https + amazonurl) != null) {
-            return{
-                // redirect to amazon smile url and append the rest of the url
-                redirectUrl : https + smileurl + url.split(amazonurl)[1]
-            };
+        // Don't try and redirect pages that will already be redirected
+        if(url.match(redirecturl) == null) {
+
+            // Check non-secure links
+            if(url.match(http + amazonurl) != null) {
+                return{
+                    // redirect to amazon smile append the rest of the url
+                    redirectUrl : http + smileurl + url.split(amazonurl)[1]
+                };
+            }
+
+            // Check secure links
+            else if (url.match(https + amazonurl) != null) {
+                return{
+                    // redirect to amazon smile url and append the rest of the url
+                    redirectUrl : https + smileurl + url.split(amazonurl)[1]
+                };
+            }
         }
     }
 }
