4b956f12d76fc5f5d66d5f76ff1d5e63df918faf
Josh haimson <jdhaimson@gmail.com>
2014-07-06 13:07:12 -0400

    Fix infite redirect loops

--- chromeplugin/request.js
+++ chromeplugin/request.js
@@ -7,35 +7,53 @@ chrome.webRequest.onBeforeRequest.addListener(function(details) {
 
 function detectRedirect(details) {
     var url = details.url;
+    
+    if (url == null) {
+        return;
+    }
+    
     var http = "http://";
     var https = "https://";
     var amazonurl = "www.amazon.com";
-    var smileurl = "smile.amazon.com";
     // ignore links with these strings in them
-    var filter = "(redirect=true)|(redirect.html)|(/gp/wishlist)|(aws.amazon.com)";
+    var filter = "(sa-no-redirect=)|(redirect=true)|(redirect.html)|(/gp/wishlist)|(aws.amazon.com)";
     
-   
-    if (url != null) {
-        
-        // Don't try and redirect pages that are in our filter
-        if(url.match(filter) == null) {
-
-            // Check non-secure links
-            if(url.match(http + amazonurl) != null) {
-                return{
-                    // redirect to amazon smile append the rest of the url
-                    redirectUrl : http + smileurl + url.split(amazonurl)[1]
-                };
-            }
-
-            // Check secure links
-            else if (url.match(https + amazonurl) != null) {
-                return{
-                    // redirect to amazon smile url and append the rest of the url
-                    redirectUrl : https + smileurl + url.split(amazonurl)[1]
-                };
-            }
-        }
+    // Don't try and redirect pages that are in our filter
+    if (url.match(filter) != null) {
+        return;
     }
+
+    if (url.match(http + amazonurl) != null) {
+        // If this is the non-secure link...
+        return redirectToSmile(http, amazonurl, url);
+
+    }  else if (url.match(https + amazonurl) != null) {
+        // If this is the secure link...
+        return redirectToSmile(https, amazonurl, url);
+    }
+
 }
 
+function redirectToSmile(scheme, amazonurl, url) {
+    var smileurl = "smile.amazon.com";
+    return {
+        // redirect to amazon smile append the rest of the url
+        redirectUrl : scheme + smileurl + getRelativeRedirectUrl(amazonurl, url)
+    };
+}
+
+function getRelativeRedirectUrl(amazonurl, url) {
+    var relativeUrl = url.split(amazonurl)[1];
+    var noRedirectIndicator = "sa-no-redirect=1";
+    var paramStart = "?";
+    var paramStartRegex = "\\" + paramStart;
+    var newurl = null;
+
+    // check to see if there are already GET variables in the url
+    if (relativeUrl.match(paramStartRegex) != null) {
+        newurl = relativeUrl + "&" + noRedirectIndicator;
+    } else {
+        newurl = relativeUrl + paramStart + noRedirectIndicator;
+    }
+    return newurl;
+}
