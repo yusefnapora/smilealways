987e9e04a27f37cd7ec502bfbb08d2616a8a2c62
Josh haimson <jdhaimson@gmail.com>
2014-09-14 16:29:22 -0400

    Update website- add FAQ

--- smilecounter/urls.py
+++ smilecounter/urls.py
@@ -7,8 +7,8 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
     # Examples:
      url(r'^$', 'smilecounter.views.home', name='home'),
-    # url(r'^faq/', 'smilecounter.views.faq', name='faq'),
-     url(r'^newredirect', 'smilecounter.views.addRedirect', name='home'),
+     url(r'^faq/', 'smilecounter.views.faq', name='faq'),
+    # url(r'^newredirect', 'smilecounter.views.addRedirect', name='home'),
     # url(r'^smilecounter/', include('smilecounter.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
